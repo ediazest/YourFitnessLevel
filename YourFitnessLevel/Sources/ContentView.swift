fc6ef9ce83583f472e29016742461ba5e148d470
Eduardo Díaz <eduardo.destrada@gmail.com>
2021-06-02 20:15:06 +0200

    Create xcode project

---
+++ YourFitnessLevel/Sources/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YourFitnessLevel
+//
+//  Created by Eduardo Díaz Estrada on 02.06.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YourFitnessLevel/Sources/YourFitnessLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourFitnessLevelApp.swift
+//  YourFitnessLevel
+//
+//  Created by Eduardo Díaz Estrada on 02.06.21.
+//
+
+import SwiftUI
+
+@main
+struct YourFitnessLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
